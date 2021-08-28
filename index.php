bf9b3c5c8f0bcfe18923d8a0882bac1cec295567
Zenku <kariyakariya399@gmail.com>
2021-08-28 02:35:16 +0100

    index page and tailwindconfig uploaded

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Zenku-f the best restaurant programme management </title>
+    <!--fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@554&display=swap" rel="stylesheet">
+    <!--end fonts-->
+    <!--styles-->
+    <link rel="stylesheet" href="style.css">
+    <!--end styles-->
+</head>
+<body>
+    <?php
+    require_once('components/navbar.php');
+    ?>
+    hello
+</body>
+</html>
